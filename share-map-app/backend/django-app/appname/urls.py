6135c38d10c7c9da9e4903b8b500298418a6eb14
KiberExpert <159645443+KiberExpert@users.noreply.github.com>
2024-08-06 10:34:32 +0500

    1

--- share-map-app/backend/django-app/appname/urls.py
+++ share-map-app/backend/django-app/appname/urls.py
@@ -3,5 +3,6 @@ from .views import *
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',index,name='index')
+    path('',index,name='index'),
+    path('login/',login,name='login')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- share-map-app/backend/django-app/appname/views.py
+++ share-map-app/backend/django-app/appname/views.py
@@ -11,3 +11,5 @@ def index(request):
     context={
     }
     return render(request, 'index.html',context)
+def login(request):
+    return render(request, 'login.html')
